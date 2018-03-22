626284765009988acd6df4e080338331b31e4767
Will Kelly <willkelly0@gmail.com>
2018-03-22 16:19:36 -0400

    update Readme, add ganache_gui network

--- truffle-config.js
+++ truffle-config.js
@@ -5,6 +5,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ganache_gui: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" 
     }
   }
 };
